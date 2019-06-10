91995b6f60535bf4ff584ee82bb94c9459ff5e47
Alejandro Morente <morente5@hotmail.es>
2019-06-11 00:12:47 +0200

    Move Events

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,8 @@ import 'hammerjs';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
-import { EventListComponent } from './event-list/event-list.component';
-import { EventDetailsComponent } from './event-details/event-details.component';
+import { EventListComponent } from './events/event-list/event-list.component';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
 
 @NgModule({
   imports: [
