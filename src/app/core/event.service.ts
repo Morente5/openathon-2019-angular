ed0b4d89e89d21e12db4f857830ffd05b682b665
Alejandro Morente <morente5@hotmail.es>
2019-06-11 00:58:12 +0200

    Environments management

--- src/app/core/event.service.ts
+++ src/app/core/event.service.ts
@@ -1,6 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+} from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +22,24 @@ export class EventService {
       'Content-Type': 'application/json'
     });
 
-    return this.http.get('assets/events.json', { headers });
+    return this.http.get(environment.apiURL, { headers }).pipe(
+      retry(3),
+      catchError(this.handleError)
+    );
+  }
+
+  // Error handling
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(`Backend returned code ${error.status}, body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError('Something bad happened; please try again later.');
   }
 }
