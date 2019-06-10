649d1ebdb9388c4e322c902d49053f9ffb6783df
Alejandro Morente <morente5@hotmail.es>
2019-06-11 00:51:12 +0200

    Consume EventService

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+import { EventService } from './event.service';
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    HttpClientModule,
+  ],
+  providers: [
+    EventService,
+  ],
 })
 export class CoreModule { }
