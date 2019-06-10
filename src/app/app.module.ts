baa53209c7d5df2ad423277a5bd3eb3bd539537b
Alejandro Morente <morente5@hotmail.es>
2019-06-11 00:02:43 +0200

    Event details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
@@ -19,6 +20,7 @@ import { EventListComponent } from './event-list/event-list.component';
     MatToolbarModule,
     MatButtonModule,
     MatListModule,
+    MatCardModule,
   ],
   declarations: [
     AppComponent,
